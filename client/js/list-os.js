521a4c54fd9ddc560a789234fc81523b237735f5
Wesley Alves <weealves1@gmail.com>
2019-08-19 20:49:37 -0300

    adição da funçao search e modificação de cores do hover dos buttons

--- client/js/list-os.js
+++ client/js/list-os.js
@@ -109,3 +109,63 @@ obterArray()
 selectList.addEventListener('change', function(){
     gerarList(this.value);
 });
+
+function buscarCliente(){
+    var total = 0;
+    var resultadoBusca = [];
+    var busca = document.getElementById("input-search");
+    if(busca.value == "" || busca.value == " " || busca.value == null) {
+        alert("Campo de Busca Vazio!");
+    }else {
+        for(var i=0; i<osArray.length;i++){
+            if(osArray[i].login==busca.value){
+                resultadoBusca.push(osArray[i]);
+                total++;
+            }
+        }
+
+        if(total!=0){
+
+            list.innerHTML="";
+
+            resultadoBusca.forEach(retorno => {
+
+                let os = document.createElement("li");
+                
+                if(retorno.statusOS=="Em Aberto") {
+                    os.style.backgroundColor = "#fbc9c9";
+                }else if(retorno.statusOS=="Finalizada"){
+                    os.style.backgroundColor = "#c2f3c3";
+                }
+
+                os.setAttribute('onclick', 'detalhes("'+osArray.indexOf(retorno)+'")');
+                
+                os.setAttribute('class','order-li');
+
+                let titulo = document.createElement("div");
+                titulo.setAttribute('class','titulo-os');
+                
+                let endereco = document.createElement("div");
+                endereco.setAttribute('class','endereco-os');
+
+                let login = document.createElement("div");
+                login.setAttribute('class','login-os');
+                
+                let plano = document.createElement("div");
+                plano.setAttribute('class','plano-os');
+                            
+                titulo.appendChild( document.createTextNode("["+retorno.statusCliente+"] "+retorno.nomeCliente) );
+                endereco.appendChild( document.createTextNode(retorno.endereco) )
+                login.appendChild( document.createTextNode("Login: "+retorno.login) );
+                plano.appendChild( document.createTextNode("Plano: "+retorno.plano) );
+                os.appendChild(titulo);
+                os.appendChild(endereco);
+                os.appendChild(login);
+                os.appendChild(plano);
+
+                list.appendChild(os);
+                
+            })
+        } 
+    }
+}
